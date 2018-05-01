ba94041d71729edc8b51bb85ac5746730fa8b6c7
estebanrodriguezo <esteban.rodriguezo@udea.edu.co>
2018-05-01 17:27:00 -0500

    Enrrutadores

---
+++ Ejercicio-1/enrutadores.cpp
@@ -0,0 +1,6 @@
+#include "enrutadores.h"
+
+Enrrutadores::Enrrutadores()
+{
+
+}
---
+++ Ejercicio-1/enrutadores.h
@@ -0,0 +1,20 @@
+#ifndef ENRRUTADORES_H
+#define ENRRUTADORES_H
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Enrutadores
+{
+public:
+    Enrutadores();
+
+    Enrutadores(string Nombre);
+    string nombreNodo;
+    vector <Enrutadores>;
+
+};
+
+#endif // ENRRUTADORES_H
